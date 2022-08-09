005b05f7a32b3d9dd2dafe5d19ddce5d6c068ca6
Jonathan Rigottier <jrigottier@gmail.com>
2022-08-09 17:59:31 +0300

    Update component for school added

---
+++ src/app/school/update-school/update-school.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import {SchoolService} from "../../shared/service/school.service";
+import {School} from "../../shared/model/school";
+import {ActivatedRoute, Router} from "@angular/router";
+import {FormBuilder, FormGroup} from "@angular/forms";
+
+@Component({
+  selector: 'app-update-school',
+  templateUrl: './update-school.component.html',
+  styleUrls: ['./update-school.component.css']
+})
+export class UpdateSchoolComponent implements OnInit {
+
+  id: any;
+
+  form: FormGroup = this.fb.group({
+    name: [null],
+    city: [null],
+    phone: [null]
+  });
+
+  constructor(private fb: FormBuilder, private schoolService: SchoolService, private router: Router,
+              private route: ActivatedRoute, private school: School) { }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.schoolService.getSchoolById(this.id).subscribe(data => {
+      this.school = data;
+      })
+
+  }
+
+  saveDetails(form: any) {
+    this.router.navigate(['/school']);
+    return this.schoolService.updateSchool(this.form.getRawValue(),this.id).subscribe(()=>{});
+
+  }
+
+}
